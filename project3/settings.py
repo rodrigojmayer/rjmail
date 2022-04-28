9c93a81640afc22a1386d878d3e87e68e947ed98
Rodrigo <rodrigojmayer@gmail.com>
2022-04-29 00:24:56 +0200

    Adjust buildpacks on Heroku

--- project3/settings.py
+++ project3/settings.py
@@ -133,7 +133,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = ' \static\ '
 
 STATICFILES_DIR = (
     os.path.join(BASE_DIR, 'static'),
