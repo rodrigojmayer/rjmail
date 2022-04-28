1e7376b47c671c66f1b26456b824b8bdc464e74c
Rodrigo <rodrigojmayer@gmail.com>
2022-04-29 00:13:58 +0200

    Adjust buildpacks on Heroku

--- project3/settings.py
+++ project3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '05$4$3aew(8ywondz$g!k4m779pbvn9)euj0zp7-ae*x@4pxr+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://rjmayl.herokuapp.com/']
+ALLOWED_HOSTS = [".herokuapp.com",".researchthroughdesign.org"]
 
 
 # Application definition
