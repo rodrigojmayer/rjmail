003bbb22c93e92c804e32acb6877ff5258650e89
Rodrigo <rodrigojmayer@gmail.com>
2022-04-29 00:25:51 +0200

    Adjust buildpacks on Heroku

--- mail/urls.py
+++ mail/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
 
     # API Routes
     path("emails", views.compose, name="compose"),
-    path("emails/<int:email_id>", views.email, name="email"),
-    path("emails/<str:mailbox>/<int:actual_page>/<int:jump_page>/<str:data_search>", views.mailbox, name="mailbox"),
+    path("emails\<int:email_id>", views.email, name="email"),
+    path("emails\<str:mailbox>\<int:actual_page>\<int:jump_page>\<str:data_search>", views.mailbox, name="mailbox"),
 ]
