9fa9d5e3b6924568ef2f3001544a394cbe947fdf
Rodrigo <rodrigojmayer@gmail.com>
2022-04-29 22:29:20 +0200

    Adjust buildpacks on Heroku4

--- project3/urls.py
+++ project3/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path(r'', admin.site.urls),
+    # path(r'', admin.site.urls),
     path('admin/ ', admin.site.urls),
     path('', include('mail.urls')),
     
