9be06bb414b1972a49b880992b38826354e38e1e
Rodrigo <rodrigojmayer@gmail.com>
2022-04-29 19:58:36 +0200

    Adjust buildpacks on Heroku4

--- project3/wsgi.py
+++ project3/wsgi.py
@@ -13,8 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project3.settings')
 
-from whitenoise.django import DjangoWhiteNoise
+# from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
 
-application = DjangoWhiteNoise(get_wsgi_application())
+# application = DjangoWhiteNoise(get_wsgi_application())
